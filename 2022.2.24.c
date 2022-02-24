00eebf68889d2b00cd723bc021eb7d9bb6e0351d
zgwaiwyd <99521061+zgwaiwyd@users.noreply.github.com>
2022-02-24 23:51:23 +0800

    Add files via upload

---
+++ 2022.2.24.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	char arr[100];
+//	scanf("%s", arr);
+//	printf("%s\n", arr);
+//	return 0;
+//
+//int main()
+//{
+//	char arr1[] = "hello,world!";
+//	char arr2[] = "hello,world!";
+//	int i = 0;
+//	while (arr1[i] == arr2[i])
+//	{
+//		if (arr1[i] == '\0')
+//		{
+//			printf("same\n");
+//			//return 0;
+//		}
+//		i++;
+//	}
+//	printf("%d\n²»Í¬", i+1);
+//	return 0;
+//}
+//system("cls")
+//pow(1,2)
+int main()
+{
+
+}
